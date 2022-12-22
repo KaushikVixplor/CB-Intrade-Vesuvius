326d0739ca9169cee0bca4b6df6b5f30d5015258
joym98 <joy.m@vixplor.com>
2022-12-22 20:04:36 +0530

    console.log ommited

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,23 +11,29 @@
 // // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {createStore,applyMiddleware,compose} from 'redux'
-import rootReducer from './store/reducers/rootReducer';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { reduxFirestore,getFirestore } from 'redux-firestore'
-import { reactReduxFirebase,getFirebase } from 'react-redux-firebase'
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const store=createStore(rootReducer,applyMiddleware(thunk));
+console.log = () => {};
+console.error = () => {};
 
-
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -35,6 +41,3 @@ ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementBy
 serviceWorker.unregister();
 
 // })
-
-
-
