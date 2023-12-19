316ad99ec4890a68e2f07f28e6955ab1a9c5d9b5
joym98 <joy.m@vixplor.com>
2023-12-19 12:25:38 +0530

    Backend encrypted config.js done

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,7 +1,7 @@
 // export const backendUrl = "http://cbmsl.ap-south-1.elasticbeanstalk.com";
 // export const backendUrl = "https://www.anisdd.com";
-// export const backendUrl = "http://localhost:8081";
-export const backendUrl = "";
+export const backendUrl = "http://localhost:8081";
+// export const backendUrl = "";
 // export const backendUrl = "http://35.154.149.214:8081";
 // export const backendUrl = 'http://Cbmsl-env.eba-2zd72thv.ap-south-1.elasticbeanstalk.com';
 export const env = "server";
